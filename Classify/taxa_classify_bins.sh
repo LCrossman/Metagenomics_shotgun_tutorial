9e20dbe4308f080bb796c16231855bef21129d40
LCrossman <lisa.crossman@gmail.com>
2026-02-02 09:04:58 +0000

    Update taxa_classify_bins.sh
    adding queue information

--- Classify/taxa_classify_bins.sh
+++ Classify/taxa_classify_bins.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
+#SBATCH --qos=bio-ds
+#SBATCH -p bio-ds
 #SBATCH --mem=30G
 #SBATCH --cpus-per-task=16
-#SBATCH --job-name=metagenomics_tutorial
+#SBATCH --job-name=metagenomics_tutorial_classification
 #SBATCH -o /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/Classify/Output_Messages/Metagenomics_tutorial-%a.out
 #SBATCH -e /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/Classify/Error_Messages/Metagenomics_tutorial-%a.err
 #SBATCH --mail-type=ALL
@@ -22,19 +24,19 @@ module add python/anaconda/2024.10/3.12.7
 echo "environment activated"
 
 # Define input/output directories
-GENOME_FASTA="/gpfs/home/hrj09fju/scratch/References/CommunityScaffolds.fasta"
+GENOME_FASTA="$HOME/scratch/References/CommunityScaffolds.fasta"
 DB_DIR="/gpfs/data/BIO-DSB/Session8/MG_workshop_2026/Classify/k2_pluspf_16GB_20251015"
-BINS_DIR=
-OUTPUT_DIR="scratch/Data/Classify"
+BINS_DIR="$HOME/scratch/Data/bins"
+OUTPUT_DIR="$HOME/scratch/Data/Classify"
 
 # Ensure OUTPUT_DIR exists
 mkdir -p "$OUTPUT_DIR"
 
-for bin in $BINS_DIR/*fasta; do 
-     kraken2 --threads 16 --db $DB_DIR $bin --quick --report --output $bin.krak ; done
+for bin in "$BINS_DIR/*fasta"; do 
+     kraken2 --threads 16 --db "$DB_DIR" "$bin" --quick --report --output "$bin.krak" ; done
 
 echo "completed classification with kraken"
-for report in $OUTPUT_DIR/*krak; do
-    bracken -d $DB_DIR -i $report -o $report.bracken -w -r 100 -l S -t 16 ; done
+for report in "$OUTPUT_DIR/*krak"; do
+    bracken -d "$DB_DIR" -i "$report" -o "$report.bracken" -w -r 100 -l S -t 16 ; done
 echo "completed report on the bins with bracken"
 
