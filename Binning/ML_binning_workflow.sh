5e49c15995de4e1c2fefffaf25885b83dfe7843c
LCrossman <lisa.crossman@gmail.com>
2026-02-02 08:52:31 +0000

    Update ML_binning_workflow.sh
    adding sbatch queue information

--- Binning/ML_binning_workflow.sh
+++ Binning/ML_binning_workflow.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
+#SBATCH --qos=bio-ds
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=metagenomics_tutorial
-#SBATCH -o /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/Binning/Output_Messages/Metagenomics_tutorial-%a.out
-#SBATCH -e /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/Binning/Error_Messages/Metagenomics_tutorial-%a.err
+#SBATCH -o scratch/Data/Metagenomics_tutorial/Binning/Output_Messages/Metagenomics_tutorial-%a.out
+#SBATCH -e scratch/Data/Metagenomics_tutorial/Binning/Error_Messages/Metagenomics_tutorial-%a.err
 #SBATCH --mail-type=ALL
+#SBATCH -j "ML_binning"
 #SBATCH --mail-user=#<your.email@address>
 
 module load mamba/25.3.1-0
@@ -12,6 +14,7 @@ module load mamba/25.3.1-0
 mamba env create -f ML_binning_env.yaml
 
 mamba activate ML_binning_env
+echo "environment successfully created"
 
 semibin2 single_easy_bin -i Mockscaffolds.fasta --environment global -b Mocked.bam.sorted.bam -o semibinout --verbose
 
