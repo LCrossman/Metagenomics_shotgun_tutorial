cb31296eaf3be2926de4ecadea0a271b916e451c
LCrossman <lisa.crossman@gmail.com>
2026-02-04 09:56:07 +0000

    Update checkm_quality.sh

--- CheckQual/checkm_quality.sh
+++ CheckQual/checkm_quality.sh
@@ -4,10 +4,10 @@
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=metagenomics_tutorial_qual
-#SBATCH -o /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/ReadAligns/Output_Messages/Metagenomics_tutorial-%a.out
-#SBATCH -e /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/ReadAligns/Error_Messages/Metagenomics_tutorial-%a.err
+#SBATCH -o /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/ReadAligns/Output_Messages/Metagenomics_tutorial-%a.out
+#SBATCH -e /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/ReadAligns/Error_Messages/Metagenomics_tutorial-%a.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=hrj09fju@uea.ac.uk
+#SBATCH --mail-user=#<your.email@address>
 
 
 module add checkm/1.2.4
