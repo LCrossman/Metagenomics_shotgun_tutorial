d3446d5c0f1e68ee9a0301157ac68914c6b1af24
LCrossman <lisa.crossman@gmail.com>
2026-02-04 09:54:42 +0000

    Update binning_workflow.sh

--- Binning/binning_workflow.sh
+++ Binning/binning_workflow.sh
@@ -5,10 +5,10 @@
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=metagenomics_tutorial
-#SBATCH -o /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/Binning/Output_Messages/Metagenomics_tutorial-%a.out
-#SBATCH -e /gpfs/home/hrj09fju/scratch/Data/Metagenomics_tutorial/Binning/Error_Messages/Metagenomics_tutorial-%a.err
+#SBATCH -o /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/Binning/Output_Messages/Metagenomics_tutorial-%a.out
+#SBATCH -e /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/Binning/Error_Messages/Metagenomics_tutorial-%a.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=$(USER)
+#SBATCH --mail-user=#<your.email@address>
 
 module add mamba/25.3.1
 source /gpfs/software/hali/mamba/25.3.1-0/etc/profile.d/mamba.sh
