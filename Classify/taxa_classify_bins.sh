75b71197cdbd9f7b8d5e98563aefa1df59c0ca33
Lisa Crossman (BIO) <hrj09fju@hali-login01.hali.private>
2026-02-11 17:58:55 +0000

    working on commands

--- Classify/taxa_classify_bins.sh
+++ Classify/taxa_classify_bins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 #SBATCH --qos=bio-ds
 #SBATCH -p bio-ds
-#SBATCH --mem=30G
-#SBATCH --cpus-per-task=16
+#SBATCH --mem=24G
+#SBATCH --cpus-per-task=8
 #SBATCH --job-name=metagenomics_tutorial_classification
 #SBATCH -o /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/Classify/Output_Messages/Metagenomics_tutorial-%a.out
 #SBATCH -e /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/Classify/Error_Messages/Metagenomics_tutorial-%a.err
@@ -17,26 +17,28 @@ module add mamba/25.3.1-0   #load mamba module to install binspreader
 
 source /gpfs/software/hali/mamba/25.3.1-0/etc/profile.d/mamba.sh
 echo "successfully sourced paths"
-
+mamba create -n unpack-env
 # initialize Mamba
-mamba activate classify_env
-module add python/anaconda/2024.10/3.12.7
+mamba activate unpack-env
+source /gpfs/data/BIO-DSB/Session7/MG_workshop_2026/Classify/classify_env/bin/activate
 echo "environment activated"
 
 # Define input/output directories
-GENOME_FASTA="$HOME/scratch/References/CommunityScaffolds.fasta"
+GENOME_FASTA="$HOME/Metagenomics_shotgun_tutorial/RawData/Communityscaffolds.fasta"
 DB_DIR="/gpfs/data/BIO-DSB/Session7/MG_workshop_2026/Classify/k2_pluspf_16GB_20251015"
-BINS_DIR="$HOME/scratch/Data/bins"
-OUTPUT_DIR="$HOME/scratch/Data/Classify"
+BINS_DIR="$HOME/Metagenomics_shotgun_tutorial/CheckQual/bins/"
+OUTPUT_DIR="$HOME/Metagenomics_shotgun_tutorial/Classify/output/"
 
 # Ensure OUTPUT_DIR exists
 mkdir -p "$OUTPUT_DIR"
+echo $DB_DIR
 
-for bin in "$BINS_DIR/*fasta"; do 
-     kraken2 --threads 16 --db "$DB_DIR" "$bin" --quick --report --output "$bin.krak" ; done
+for bin in "$BINS_DIR*fasta"; do
+     echo $bin;	
+     kraken2 --db "$DB_DIR" --memory-mapping --threads 8 $bin --report $bin.krak --output $bin.out ; done
 
 echo "completed classification with kraken"
 for report in "$OUTPUT_DIR/*krak"; do
-    bracken -d "$DB_DIR" -i "$report" -o "$report.bracken" -w -r 100 -l S -t 16 ; done
+    bracken -d "$DB_DIR" -i $report -o $report.bracken -w -r 100 -l S -t 16 ; done
 echo "completed report on the bins with bracken"
 
