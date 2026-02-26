bac7f071d2bcc95184bcc1d2d0f321bd38906f16
LCrossman <lisa.crossman@gmail.com>
2026-02-26 10:31:53 +0000

    changing output & error message dir so it does not overwrite the previous binning workflow

--- Binning/ML_binning_workflow.sh
+++ Binning/ML_binning_workflow.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=metagenomics_tutorial_MLbinning
-#SBATCH -o /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/Binning/Output_Messages/Metagenomics_tutorial-%a.out
-#SBATCH -e /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/Binning/Error_Messages/Metagenomics_tutorial-%a.err
+#SBATCH -o /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/MLBinning/Output_Messages/Metagenomics_tutorial-%a.out
+#SBATCH -e /gpfs/home/%u/scratch/Data/Metagenomics_tutorial/MLBinning/Error_Messages/Metagenomics_tutorial-%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=#<your.email@address>
 
